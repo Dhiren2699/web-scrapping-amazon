cc5e625099ad02f11dd742eb4026e5083859fbe3
Dhiren Boricha <48174979+Dhiren2699@users.noreply.github.com>
2020-01-02 13:45:36 +0530

    Update web_scrap.py

--- web_scrap.py
+++ web_scrap.py
@@ -1,3 +1,9 @@
+# pip install scikit-image
+# pip install requests
+# pip install beautifulsoup4
+
+
+from skimage import io
 import requests
 from bs4 import BeautifulSoup
 
@@ -11,29 +17,36 @@ product = requests.get('https://www.amazon.in/s?k={0}&rh=p_36%3A{1}00-{2}00&s=re
 soup = BeautifulSoup(product.content, features="html")
 
 #a = soup.find_all("div",class_='a-section aok-relative s-image-fixed-height')
-#print(a)  # for laptops and mobiles divison class
 
 print('\n')
-
+print('The Products Best For You according to your Budget are :\n')
 
 detail= soup.find_all('img',class_='s-image')
 alt = []
 for everyitem in detail:
     alt.append(everyitem['alt'])
-#print(alt)
+
 src = []
 for everyitem in detail:
     src.append(everyitem['src'])
-#print(alt)
+
 
 detail2 = soup.find_all('span',class_='a-price-whole')
 
 price = []
 for everyitem in detail2:
     price.append(everyitem.get_text())
-
-for result in range(0,10):
-    print('Name of The Product : ' +alt[result])
-    print('Price : '+price[result], 'RS.')
-    print('Image of the Product : '+src[result])
+i=1
+for result in range(0,18):
+     
+    print(+i,'. Name of The Product : ' +alt[result])
+    print('    Price : '+price[result], 'RS.')
+    print('    Image of Product : ')
+    j = src[result]
+    io.imshow(io.imread(j))
+    io.show()
+    
     print('\n')
+    i=i+1
+    
+
